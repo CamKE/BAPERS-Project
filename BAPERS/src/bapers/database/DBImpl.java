ca417ebddc2edbf30d0719c0396fa11a26f30624
CamKE <37081579+CamKE@users.noreply.github.com>
2018-03-06 23:03:26 +0000

    Login system, packages added

--- BAPERS/src/bapers/database/DBImpl.java
+++ BAPERS/src/bapers/database/DBImpl.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package bapers;
+package bapers.database;
 
 /**
  *
  * @author CameronE
  */
-public class Controller {
+public class DBImpl {
     
 }
--- BAPERS/src/bapers/user/UserDetails.java
+++ BAPERS/src/bapers/user/UserDetails.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package bapers;
+package bapers.user;
 
 /**
  *
  * @author CameronE
  */
-public class DBImpl {
+public class UserDetails {
     
 }
