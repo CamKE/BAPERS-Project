c11660ac439ea8a9fc13cc2ebb48ade6d47dd765
CamKE <37081579+CamKE@users.noreply.github.com>
2018-03-17 16:27:52 +0000

    run this script in workbench

---
+++ BAPERS-departments-script.sql
@@ -0,0 +1,3 @@
+use bapers_data;
+
+insert into department(department_code,department_name) values('CR','copy room'),('DR','dark room'),('DA','development area'),('PR','printing room'),('FR','finishing room'),('PD','packaging department');
