0ee29a57adb626e6db0a1272e611f18c9fd3756e
CamKE <37081579+CamKE@users.noreply.github.com>
2018-03-17 16:43:11 +0000

    run these

--- BAPERS-departments-script.sql
+++ BAPERS-departments-script.sql
@@ -1,3 +1,6 @@
 use bapers_data;
 
 insert into department(department_code,department_name) values('CR','copy room'),('DR','dark room'),('DA','development area'),('PR','printing room'),('FR','finishing room'),('PD','packaging department');
+
+ALTER TABLE task
+MODIFY  COLUMN duration_min smallint;
