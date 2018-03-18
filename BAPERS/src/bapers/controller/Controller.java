6d60949516badfe2a951b74f0620265e62ce56fa
CamKE <37081579+CamKE@users.noreply.github.com>
2018-03-18 20:15:00 +0000

    user search

--- BAPERS/src/bapers/controller/Controller.java
+++ BAPERS/src/bapers/controller/Controller.java
@@ -22,21 +22,49 @@ public class Controller {
         database = new DBImpl();
         conn = database.connect();
     }
-    
-   public boolean findUser(String userNumber,String firstName,String lastName,String role)
-   {      
-       String sql = "SELECT from User WHERE ";
 
-       if (!userNumber.isEmpty())
-       {
-           sql = sql.concat("account_no = '" + Integer.parseInt(userNumber) + "");
-       } else {
-           
-       }
-       
-       ResultSet rs = database.read(sql, conn);
-       System.out.println("yo");
-       return false;
-   }
- 
+    public boolean findUser(String userNumber, String firstName, String lastName, String role) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("SELECT * from user WHERE 1=1 ");
+
+        if (!userNumber.isEmpty()) {
+            sb.append("AND account_no = ").append(Integer.parseInt(userNumber));
+        } else {
+            boolean a = !firstName.isEmpty();
+            boolean b = !lastName.isEmpty();
+            boolean c = !role.equals("Any");
+
+            if (!firstName.isEmpty()) {
+                sb.append(" AND firstname = '").append(firstName).append("'");
+            }
+            if (!lastName.isEmpty()) {
+                sb.append(" AND lastname = '").append(lastName).append("'");
+            }
+            if (!role.equals("Any")) {
+                int role_id = 0;
+                switch (role) {
+                    case "Office Manager":
+                        role_id = 1;
+                        break;
+                    case "Shift Manager":
+                        role_id = 2;
+                        break;
+                    case "Receptionist":
+                        role_id = 3;
+                        break;
+                    case "Technician":
+                        role_id = 4;
+                        break;
+//                    default: // Optional
+//                    // Statements
+                }
+                sb.append(" AND Role_role_id = ").append(role_id);
+            }
+        }
+        System.out.println(sb.toString());
+        ResultSet rs = database.read(sb.toString(), conn);
+        System.out.println("yo");
+        return false;
+    }
+
 }
