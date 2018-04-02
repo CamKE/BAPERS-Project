5c6abf26e2cf6a82af7eb4273402a3ff5fca0156
KelvinHW <Ho.Wong.2@city.ac.uk>
2018-04-02 18:24:46 +0100

    First creation of auto backup config class

---
+++ BAPERS/src/bapers/acct/AutoBackupConfig.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bapers.acct;
+
+/**
+ *
+ * @author kelvin
+ */
+public class AutoBackupConfig {
+    private final String backupMode;
+    private final String backupFrequency;
+    private final String backupLocation;
+
+    public AutoBackupConfig(String backupMode, String backupFrequency, String backupLocation) {
+        this.backupMode = backupMode;
+        this.backupFrequency = backupFrequency;
+        this.backupLocation = backupLocation;
+    }
+
+    public String getBackupMode() {
+        return backupMode;
+    }
+
+    public String getBackupFrequency() {
+        return backupFrequency;
+    }
+
+    public String getBackupLocation() {
+        return backupLocation;
+    }
+    
+}
