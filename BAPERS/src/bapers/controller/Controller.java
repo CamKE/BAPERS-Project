3dc611fdee4888b4c72935d199efa4f84e5be363
KelvinHW <Ho.Wong.2@city.ac.uk>
2018-04-07 16:25:54 +0100

    Updated controller for auto update and the difference between updating and inserting the auto config data. Also choosing between paying on time and late.

--- BAPERS/src/bapers/controller/Controller.java
+++ BAPERS/src/bapers/controller/Controller.java
@@ -12,6 +12,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  *
@@ -70,48 +71,91 @@ public class Controller {
     
     
     
+    // auto backup
+    public boolean checkAutoBackupConfigExist() {
+        final String sql = "select * from BackupSettings;";
+        int size = 0;
+        
+        try (ResultSet result = database.read(sql, conn)) {
+            while (result.next()) ++size;
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+        return size > 0;
+    }
     
     public AutoBackupConfig getAutoBackupConfigData() {
-        String sql = "select * from BackupSettings";
+        final String sql = "select * from BackupSettings;";
         AutoBackupConfig autoData = null;
         
         try (ResultSet result = database.read(sql, conn)) {
-            result.next();
-            autoData = new AutoBackupConfig(result.getString("backup_mode"), result.getString("backup_frequency"), result.getString("backup_location"));
-            
-            System.out.println(autoData.getBackupMode());
-            System.out.println(autoData.getBackupFrequency());
-            System.out.println(autoData.getBackupLocation());
+            result.next(); // gets only the first row in the table
+            autoData = new AutoBackupConfig(
+                    result.getString("backup_mode"), 
+                    result.getString("backup_frequency"), 
+                    result.getString("backup_location")
+            );
         } catch (SQLException ex) {
             System.out.println(ex);
         }
         return autoData;
     }
     
-    public void setAutoBackupConfig(AutoBackupConfig config) {
-        String sql = "INSERT INTO `bapers_data`.`BackupSettings` (`backup_mode`, `backup_frequency`, `backup_location`) VALUES (" 
-                + "\'" + config.getBackupMode().toLowerCase() + "\', " 
-                + "\'" + config.getBackupFrequency().toLowerCase() + "\', " 
-                + "\'" + config.getBackupLocation() + "\'" + ");";
-        
-        try {
-            database.write(sql, conn);
-            System.out.println("Complete");
-        } catch (Exception e) {
-            System.out.println("Exception error: " + e);
+    public void setAutoBackupConfig(final AutoBackupConfig config) {
+        if (checkAutoBackupConfigExist() == false) {
+            final String sql = "insert into `bapers_data`.`BackupSettings` (`backup_mode`, `backup_frequency`, `backup_location`) values (" 
+                    + "\'" + config.getBackupMode().toLowerCase() + "\', " 
+                    + "\'" + config.getBackupFrequency().toLowerCase() + "\', " 
+                    + "\'" + config.getBackupLocation() + "\'" + ");";
+
+            try {
+                database.write(sql, conn);
+            } catch (Exception e) {
+                System.out.println("Exception error: " + e);
+            }
+        } else {
+            final AutoBackupConfig oldConfigData = getAutoBackupConfigData();
+            final String sql = "UPDATE `BAPERS_data`.`BackupSettings` SET"
+                    + "`backup_mode`=" + "\'" + config.getBackupMode() + "\', " 
+                    + "`backup_frequency`=" + "\'" + config.getBackupFrequency() + "\', " 
+                    + "`backup_location`=" + "\'" + config.getBackupLocation() + "\'" 
+                    + "WHERE `backup_mode`=" + "\'" + oldConfigData.getBackupMode() + "\'"
+                    + ";";
+
+            try {
+                database.write(sql, conn);
+            } catch (Exception e) {
+                System.out.println("Exception error: " + e);
+            }
         }
     }
     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //late payment
     public ArrayList<Invoice> getInvoices() throws ParseException {
-        String sql = "select * from Invoice";
+        final String sql = "select * from Invoice";
         
         // putting data into array list
-        ArrayList<Invoice> invoices = new ArrayList<>();
+        final ArrayList<Invoice> invoices = new ArrayList<>();
         try (ResultSet result = database.read(sql, conn)) {
             while (result.next()) {
-                Invoice invoice = new Invoice(result.getInt("Invoice_no"), result.getInt("Job_job_no"), 
-                result.getInt("total_payable"), result.getDate("date_issued"), result.getString("invoice_status"),
-                result.getString("invoice_location"));
+                Invoice invoice = new Invoice(
+                        result.getInt("Invoice_no"), 
+                        result.getInt("Job_job_no"), 
+                        result.getInt("total_payable"), 
+                        result.getDate("date_issued"), 
+                        result.getString("invoice_status"),
+                        result.getString("invoice_location")
+                );
                 if(invoice.getInvoiceStatus().equals(Invoice.Status.AWAITINGPAYMENT))
                     invoices.add(invoice);
             }
@@ -121,19 +165,21 @@ public class Controller {
         return invoices;
     }
     
-    public void recordPayment(Payment p, String type) {
-        String sql;
+    public void recordPayment(final Payment p, final String type, final Invoice i, final Card card) {
+        boolean paymentRecorded;
+        
         if (type.equals("Card")) {
+            recordCardDetails(p, i, card);
+            
             final int paymentNo = p.getPaymentNo();
             final double total = p.getTotal();
             final String paymentType = p.getPaymentType();
-            String paymentDate =  p.getPaymentDate();
+            final Date paymentDate =  p.getPaymentDate();
             final int invoiceNumber = p.getInvoiceNumber();
-            //final String cardType = ((PaymentCard) p).getCardType();
-            final String cardDetailsLast4digits = ((PaymentCard) p).getCardDetailsLast4digits();
+            final String cardDetailsLast4digits = ((PaymentCard) p).cardDetailsLast4Digits();
             final String cardDetailsExpiryDate = ((PaymentCard) p).getCardDetailsExpiryDate();
             
-            sql = "INSERT INTO PaymentRecord VALUES (" 
+            final String sql = "insert into PaymentRecord values (" 
                     + paymentNo 
                     + ',' + total 
                     + ", \"" + paymentType 
@@ -141,7 +187,6 @@ public class Controller {
                     + ',' + invoiceNumber + ","
                     + "\"" +  cardDetailsLast4digits
                     + "\"," + "\"" + cardDetailsExpiryDate + "\"" + ");";
-            //System.out.println(sql);
             
             try {
                 database.write(sql, conn);
@@ -149,27 +194,32 @@ public class Controller {
                 System.out.println("Exception error: " + e);
             }
             
-            String sql2 = "UPDATE bapers_data.invoice SET invoice_status='Paid on time' WHERE `Invoice_no`= " 
-                    + '\'' + paymentNo + '\'' +";" ;
-            try {
-                database.write(sql2, conn);
-            } catch (Exception e) {
-                System.out.println("Exception error: " + e);
-            }
-        } else if (type.equals("Cash")) {
+            paymentRecorded = true;
+            changeInvoiceStatus(paymentRecorded, p, i);
+        }
+    }
+    
+    public void recordPayment(final Payment p, final String type, final Invoice i) {
+        String sql;
+        boolean paymentRecorded;
+        
+        if (type.equals("Cash")) {
             final int paymentNo = p.getPaymentNo();
             final double total = p.getTotal();
             final String paymentType = p.getPaymentType();
-            String paymentDate =  p.getPaymentDate();
+            final Date paymentDate =  p.getPaymentDate();
             final int invoiceNumber = p.getInvoiceNumber();
             
-            sql = "INSERT INTO PaymentRecord VALUES (" 
+            sql = "insert into PaymentRecord values (" 
                     + paymentNo 
                     + ',' + total 
                     + ", \"" + paymentType 
                     + "\"," + '\'' + paymentDate + '\''
-                    + ',' + invoiceNumber + ");";
-            System.out.println(sql);
+                    + ", " + invoiceNumber 
+                    + ", "  + null
+                    + ", "  + null
+                    + ");"
+                    ;
             
             try {
                 database.write(sql, conn);
@@ -177,13 +227,87 @@ public class Controller {
                 System.out.println("Exception error: " + e);
             }
             
-            String sql2 = "UPDATE bapers_data.invoice SET invoice_status='Paid on time' WHERE `Invoice_no`= " 
-                    + '\'' + paymentNo + '\'' +";" ;
+            paymentRecorded = true;
+            changeInvoiceStatus(paymentRecorded, p, i);
+        }
+    }
+    
+    public void recordCardDetails(final Payment p, final Invoice invoice, final Card card) {
+        final boolean cardRecorded = checkCardAlreadyRecorded(card);
+        
+        if (cardRecorded == false) {
+            String sql = "insert into CardDetails values(" + "\'"
+                    + ((PaymentCard) p).cardDetailsLast4Digits()+ "\', \'"
+                    + ((PaymentCard) p).getCardType() + "\', \'"
+                    + ((PaymentCard) p).getCardDetailsExpiryDate() + "\', \'" 
+                    + findAssociatedCustomerFromInvoce(invoice) + "\'"
+                    + ");"
+                    ;
+
             try {
-                database.write(sql2, conn);
+                database.write(sql, conn);
             } catch (Exception e) {
                 System.out.println("Exception error: " + e);
             }
         }
+    }
+    
+    public String findAssociatedCustomerFromInvoce(final Invoice invoice) {
+        String customer = "";
+        final String sql = "select * from Job where job_no = " + invoice.getJobJobNo();
+        
+        try (ResultSet result = database.read(sql, conn)) {
+            result.next();
+            customer = result.getString("Customer_account_no");
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+        
+        return customer;
+    }
+    
+    public boolean checkCardAlreadyRecorded(final Card card) {
+        boolean cardRecorded = false;
+        final String sql = "select count(*) from CardDetails where last4digits = \'" 
+                + card.getLast4Digits() + "\' and card_type = \'"
+                + card.getCardType() + "\' and expiry_date = \'"
+                + card.getExpiryDate() + "\';"
+                ;
+        
+        try (ResultSet result = database.read(sql, conn)) {
+            result.next();
+            cardRecorded = result.getBoolean("count(*)");
+            //System.out.println(cardRecorded);
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+        
+        return cardRecorded;
+    }
+    
+    public void changeInvoiceStatus(final boolean paymentRecorded, final Payment p, final Invoice i) {
+        if (paymentRecorded == true) {
+            final String status = paidOnTIme(p, i);
+            final String sql = "update bapers_data.invoice set invoice_status = \'"
+                    + status
+                    + "\' where `Invoice_no` = " + '\'' 
+                    + p.getPaymentNo() + '\'' + ";"
+                    ;
+            try {
+                database.write(sql, conn);
+            } catch (Exception e) {
+                System.out.println("Exception error: " + e);
+            }
+        }
+    }
+    
+    public String paidOnTIme(final Payment p, final Invoice i) {
+        String invoiceStatus;
+        
+        if (p.getPaymentDate().compareTo(i.getDateIssued()) > 0)
+            invoiceStatus = "Paid late";
+        else 
+            invoiceStatus = "Paid on time";
+        return invoiceStatus;
     }
 }
