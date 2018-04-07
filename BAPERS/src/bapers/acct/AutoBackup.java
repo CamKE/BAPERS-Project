518a8208e0a1b7e5dc153a19cfc14fc1b91a66c3
KelvinHW <Ho.Wong.2@city.ac.uk>
2018-04-07 16:22:21 +0100

    Auto Backup class modified to allow both mac and windows to backup

--- BAPERS/src/bapers/acct/AutoBackup.java
+++ BAPERS/src/bapers/acct/AutoBackup.java
@@ -11,6 +11,7 @@ import java.util.Calendar;
 import java.util.TimerTask;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -18,31 +19,48 @@ import java.util.logging.Logger;
  */
 public class AutoBackup extends TimerTask {
 
-    AutoBackupConfig test;
-    public AutoBackup(AutoBackupConfig test) {
-        this.test = test;
+    AutoBackupConfig configData;
+    Calendar date;
+    
+    public AutoBackup(AutoBackupConfig configData, Calendar date) {
+        this.configData = configData;
+        this.date = date;
     }
     
     @Override
     public void run() {
-        //System.out.println("Done");
+        System.out.println(date.get(Calendar.DAY_OF_WEEK));
         try {
             Runtime runtime = Runtime.getRuntime();
-            
+
             String date = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss").format(Calendar.getInstance().getTime());
             date = date.replace(' ', '_');
             date = date.replace(':', '-');
-            String filename = "BAPERS_" + date + ".sql";
-            //System.out.println(filename);
-            
-            int processComplete;
-            String dump = "/usr/local/mysql-5.7.21-macos10.13-x86_64/bin/mysqldump -uroot -pPassword --databases bapers_data > " + test.getBackupLocation() +"/" + filename;
-            String[] cmdarray = {"/bin/sh","-c", dump};
-            Process p = Runtime.getRuntime().exec(cmdarray);
-            processComplete = p.waitFor();
-            
+            final String filename = "BAPERS_" + date + ".sql";
+                
+            int processComplete = -1;
+            Process p;
+            String dump;
+
+            final String os = System.getProperty("os.name");
+                
+            switch(os) {
+                case "Mac OS X" :
+                    dump = "/usr/local/mysql-5.7.21-macos10.13-x86_64/bin/mysqldump -uroot -pPassword --databases bapers_data > " + configData.getBackupLocation() +"/" + filename;
+                    String[] cmdarray = {"/bin/sh","-c", dump};
+                    p = Runtime.getRuntime().exec(cmdarray);
+                    processComplete = p.waitFor();
+                    break;
+                
+                case "Windows 10" : 
+                    dump = "/usr/local/mysql-5.7.21-macos10.13-x86_64/bin/mysqldump -uroot -ppassword -B bapers_data -r \"" + configData.getBackupLocation() + "\\" + filename + "\"";
+                    p = runtime.exec(dump);
+                    processComplete = p.waitFor();
+                    break;
+            }
+                
             if (processComplete == 0)
-                System.out.println("Complete auto backup");
+                JOptionPane.showMessageDialog(null, "Complete auto backup");
             else
                 System.out.println("Failed auto backup");
         } catch (InterruptedException | IOException ex) {
