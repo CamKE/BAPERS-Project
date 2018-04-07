10c7dc77c0fe38b0376cffbbb3ee6412b957a64a
KelvinHW <Ho.Wong.2@city.ac.uk>
2018-04-07 16:20:26 +0100

    Card class

---
+++ BAPERS/src/bapers/acct/Card.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bapers.acct;
+
+/**
+ *
+ * @author kelvin
+ */
+public class Card {
+    private final String last4Digits;
+    private final String cardType;
+    private final String expiryDate;
+
+    public Card(String last4Digits, String cardType, String expiryDate) {
+        this.last4Digits = last4Digits;
+        this.cardType = cardType;
+        this.expiryDate = expiryDate;
+    }
+
+    public String getLast4Digits() {
+        return last4Digits;
+    }
+
+    public String getCardType() {
+        return cardType;
+    }
+
+    public String getExpiryDate() {
+        return expiryDate;
+    }
+}
